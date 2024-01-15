bfb509c5dbbe3b8970d1bbe6f12a4bf0f453acfb
nyymy <tigeranik@gmail.com>
2024-01-15 19:36:31 +0200

    message_creater commit

--- telegram_bot/Utils/Message_creater.py
+++ telegram_bot/Utils/Message_creater.py
@@ -1,5 +1,6 @@
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import pandas as pd
 import openmeteo_requests
 import requests_cache
 import seaborn as sns
@@ -42,7 +43,6 @@ class MessageCreator:
         precipitation_probability_data = []
         time_slots = []
         current_day = None
-        print(self.__dataframe_rounded.temperature_2m)
         for index, row in self.__dataframe_rounded.iterrows():
             # print(row)
             date = row['date']
@@ -66,35 +66,7 @@ class MessageCreator:
                 formatted_data = message
             hourly_data_formatted.append(formatted_data)
             formatted_output = '\n'.join(hourly_data_formatted)
-            temperature_data.append(temperature)
-            time_slots.append(date)
-
-        sns.set(style="whitegrid")
-
-        # Plotting the temperature and precipitation probability
-        plt.figure(figsize=(10, 5))
-
-        plt.plot(time_slots, temperature_data, marker='o', linestyle='-', color='b', label='Temperature')
-
-
-        plt.title('Forecast for 3 days')
-        plt.xlabel('')  # Empty string to remove x-axis label
-        plt.ylabel('Temperature (°C)')
-        plt.legend()
-
-        # Format x-axis as time with a custom format and rotate labels vertically
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-        plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=4))
-
-        # Set font size for x-axis labels
-        plt.xticks(fontsize=10)
-
-        # Save the plot or display it as needed
-        plt.savefig('forecast_plot.png')  # Save the plot to a file
-        plt.show()  # Display the plot
-
-        formatted_output = '\n'.join(hourly_data_formatted)
-        return formatted_output
+        self.create_graphic()
 
         return formatted_output
 
@@ -119,5 +91,45 @@ class MessageCreator:
             daily_data_formatted.append(formatted_data)
             formatted_output = '\n'.join(daily_data_formatted)
 
-
         return formatted_output
+
+    def create_graphic(self):
+        df = self.__dataframe_rounded
+        df['date'] = pd.to_datetime(df['date'])
+
+        # Plotting using Matplotlib with line and circular markers
+        plt.plot(df['date'], df['temperature_2m'], linestyle='-', marker='o', markersize=5, color='b')
+
+        # Formatting the date ticks with hours and minutes vertically
+        plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=4))
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+
+        # Set font size for hours and minutes using tick_params for both top and bottom
+        plt.tick_params(axis='x', which='major', labelsize=7, bottom=True, top=True)
+
+        plt.xticks(rotation='vertical')
+
+        # Add a marker for a new day with day and month
+        for _, day_date in df.groupby(df['date'].dt.date)['date']:
+            plt.axvline(day_date.iloc[0], color='gray', linestyle='--', linewidth=0.5)
+            plt.text(day_date.iloc[0], plt.ylim()[1] - 0.5, day_date.iloc[0].strftime('%b %d'),
+                     ha='center', va='bottom', rotation=0, color='black',
+                     size=9, bbox=dict(facecolor='white', edgecolor='none', boxstyle='round4'))
+
+        # Set font size for the y-axis label
+        plt.ylabel('Temperature (°C)', fontsize=10)
+
+        # Adding title
+        plt.title('Forecast for 3 days')
+
+        # Display the grid inside the graph
+        plt.grid(color='lightgray', linestyle='--', linewidth=0.5, which='both', axis='both', alpha=0.5)
+
+        # Move the x-axis to the top
+        plt.gca().xaxis.set_ticks_position('top')
+
+        # Adding a legend
+        plt.legend()
+
+        # Display the plot
+        plt.show()
